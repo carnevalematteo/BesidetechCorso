1692241b76de472ab69aa75b7c049dcaeb49db8b
carnevale matteo <mcarnevale@pccube.com>
2022-05-11 10:03:14 +0200

    solution exercise + generate comment

---
+++ src/FilterW.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+
+public class FilterW {
+
+    /**
+     * questo medoto dato un array in ingresso filtra lo stesso per creare un nuovo
+     * array nel quale verràno inserite eventuali parole ripetute.
+     * @author matteo carnevale.
+     * @param parole rappresenta l'array contenente le parole.
+     * @return un array contenente esclusivamente le parole doppioni senza ripetizioni.
+     */
+    public void filterWord(String[] parole){
+
+        String [] foundWord= new String[parole.length/2];
+
+        String lastWord="";
+
+        String result[];
+
+        int count =0;
+
+        for(int i=0,j=1;i<parole.length && j< parole.length-1;i++,j++){
+
+            if(parole[i].equals(parole[i+1])){
+
+                lastWord=parole[i];
+
+                for(String s : parole){
+
+                    if (s.equals(lastWord)){
+
+                        s="";
+                    }
+                }
+
+
+                foundWord[count]=lastWord;
+                count++;
+
+            }
+
+        }
+
+        int pos = 0;
+        result = new String[count];
+
+        for(int i =0 ;i< foundWord.length;i++){
+
+            if(foundWord[i]!=null){
+
+                result[pos]=foundWord[i];
+
+                pos++;
+            }
+        }
+
+        System.out.println(Arrays.toString(result));
+    }
+
+}
