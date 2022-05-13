48eb1753bad25ab574515ccfeb591699eaefd2e2
carnevale matteo <mcarnevale@pccube.com>
2022-05-13 16:05:45 +0200

    solution exercise

--- src/com/corso/MapExercise.java
+++ src/com/corso/MapExercise.java
@@ -10,6 +10,8 @@ public class MapExercise {
     public static void main(String[] args) {
         List<String> list1 = new ArrayList<>();
         list1.add("gaetano");
+        list1.add("gaetano");
+        list1.add("gaetano");
         list1.add("ettore");
         list1.add("matteo");
         list1.add("paolo");
@@ -24,27 +26,60 @@ public class MapExercise {
         list1.add("gaetano");
         list1.add("matteo");
         list1.add("mike");
-        list1.add("gaetano");
         list1.add("matteo");
         list1.add("paolo");
-        list1.add("gaetano");
         list1.add("mike");
 
 
         Map<String, Integer>  result = getAverage(list1);
 
         System.out.println(result);
+
+            Integer [] num ={1,2,3,4};
+            Long [] numLong={45465456l,4566546456l,45646554l};
+            String [] strings ={"A","B","C"};
+
+            printArray(num);
+            printArray(numLong);
+            printArray(strings);
+
+
+    }
+
+    public static <E> void printArray( E[] inputArray ) {
+        for(E element : inputArray) {
+            System.out.println(element);
+        }
+
     }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static Map<String, Integer> getAverage(List<String> list1) {
+
+        //gaetano , 2
         Map<String, Integer> result = new HashMap<>();
 
         for (String s : list1) {
 
             if (result.containsKey(s)) {
+
                 int currentValue = result.get(s);
                 result.put(s, currentValue + 1);
+
             } else {
 
                 result.put(s, 1);
