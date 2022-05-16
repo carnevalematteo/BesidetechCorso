c0ecede16992468f285dde933af83e2dde7f7aa9
matte <carnevalemg1989@libero.it>
2022-05-16 12:55:05 +0200

    file

--- src/com/corso/file/ScriviLeggi.java
+++ src/com/corso/file/ScriviLeggi.java
@@ -1,33 +1,64 @@
 package com.corso.file;
 
 
+import java.awt.*;
+import java.io.*;
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
+import java.util.Properties;
 
 public class ScriviLeggi {
 
 public void write(String sentence) throws IOException {
-    String trainingFullPath = "C:\\Users\\carne\\db1.txt";
-    String contentToAdd = "\r\n" + sentence;
-    Files.write(
-            Paths.get(trainingFullPath),
-            contentToAdd.getBytes(),
-            StandardOpenOption.APPEND);
+    String trainingFullPath = "C:\\Users\\matte\\split.txt";
+        String contentToAdd = "\r\n" + sentence;
+        Files.write(
+                Paths.get(trainingFullPath),
+                contentToAdd.getBytes(),
+                StandardOpenOption.APPEND);
 
 
 }
 
-
     public  void readItem() throws IOException {
-        String trainingFullPath = "C:\\Users\\carne\\db1.txt";
+        String trainingFullPath = "C:\\Users\\matte\\db1.txt";
         List<String> lines =Files.readAllLines(Paths.get(trainingFullPath));
-
         for(String s : lines) {
             System.out.println(s);
         }
     }
+
+
+    public void splitReadFile() throws IOException {
+        String trainingFullPath = "C:\\Users\\matte\\split.txt";
+        List<String> content = Files.readAllLines(Paths.get(trainingFullPath));
+        //a,b,c,d,e
+        String result ="";
+        for(String s : content){
+            result = result.concat(s);
+        }
+
+        String [] word = result.split(",");
+
+        for(String s : word){
+            System.out.println(s);
+        }
+
+    }
+
+
+    public String getConfig(String daRicercare) throws IOException {
+        Properties conf = new Properties();
+        String trainingFullPath = "C:\\Users\\matte\\split.txt";
+        InputStream is =new FileInputStream(trainingFullPath);
+        conf.load(is);
+        String result = conf.getProperty(daRicercare,"non trovato");
+        return result;
+
+
+    }
+
 }
